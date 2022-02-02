5860d744ea91cda072b2af5012966896f163f166
khadim Niass <niassssn@gmail.com>
2022-02-02 18:58:54 +0000

    embelissement de l'affichage de la fonction

--- exercice6/fonctions.php
+++ exercice6/fonctions.php
@@ -8,10 +8,12 @@ function delta($n1, $n2, $n3)
 function second_degre($a, $b, $c)
 {
     if ($a == 0) {
-        echo "l'equation n'est pas du second degres \n";
+        echo "l'equation n'est pas du second degres car a est null\n";
+        
         if ($b == 0) {
             if ($c == 0) {
-                echo "infinité de solution \n";
+                echo "infinité de solution ; ";
+                echo "car vous avez mis tous les coefficients null \n";
             } else {              #c!=0
                 echo "pas de solution \n";
             }
@@ -21,14 +23,15 @@ function second_degre($a, $b, $c)
     } else {  #a!=0
         $d = delta($a, $b, $c);
         if ($d < 0) {
-            echo "pas de solution \n";
+            echo "pas de solution ";
+            echo "car Le descriminents est négatif \n";
         }elseif($d==0){
+            echo "on a une solution : \n";
             echo "x0 = ".-1*$b/2*$a ."\n";
         }else {
+            echo "on a deux solutions : \n";
             echo "x1 = ".(-1*$b-sqrt($d))/2*$a ."\n";
             echo "x2 = ".(-1*$b+sqrt($d))/2*$a ."\n";
         }
     }
 }
-
-second_degre(0,0,0);
