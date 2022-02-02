8aff72dc04db8d2d81b570513a3fecd441ce678b
khadim Niass <niassssn@gmail.com>
2022-02-02 18:54:05 +0000

    fonction du seges pret

---
+++ exercice6/fonctions.php
@@ -0,0 +1,34 @@
+<?php
+
+function delta($n1, $n2, $n3)
+{
+    return $n2 * $n2 - 4 * $n1 * $n3;
+}
+
+function second_degre($a, $b, $c)
+{
+    if ($a == 0) {
+        echo "l'equation n'est pas du second degres \n";
+        if ($b == 0) {
+            if ($c == 0) {
+                echo "infinité de solution \n";
+            } else {              #c!=0
+                echo "pas de solution \n";
+            }
+        } else {                  #b!=0
+            $solution = -1 * $c / $b;
+        }
+    } else {  #a!=0
+        $d = delta($a, $b, $c);
+        if ($d < 0) {
+            echo "pas de solution \n";
+        }elseif($d==0){
+            echo "x0 = ".-1*$b/2*$a ."\n";
+        }else {
+            echo "x1 = ".(-1*$b-sqrt($d))/2*$a ."\n";
+            echo "x2 = ".(-1*$b+sqrt($d))/2*$a ."\n";
+        }
+    }
+}
+
+second_degre(0,0,0);
---
+++ exercice6/form.html.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>saisi des donnees pour second degres</title>
+
+</head>
+
+<body>
+    <form action="" method="get">
+        <div>
+            <label for="a">a : </label>
+            <input id="a" name="a" type="number">
+        </div>
+
+        <div>
+            <label for="b">b : </label>
+            <input id="b" name="b" type="number">
+        </div>
+
+        <div>
+            <label for="c">c : </label>
+            <input id="c" name="c" type="number">
+        </div>
+        
+        <div>
+            <input type="button" value="calculer">
+        </div>
+    </form>
+</body>
+
+</html>
