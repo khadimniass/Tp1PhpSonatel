7e6bc7a047b9d64824d97c72d1990885382aa812
khadim Niass <niassssn@gmail.com>
2022-02-03 22:11:02 +0000

    stylisation du formulaire exo6 et retourner le resultat dans index.php

--- exercice6/form.html.php
+++ exercice6/form.html.php
@@ -2,34 +2,87 @@
 <html lang="en">
 
 <head>
+    <style>
+        body {}
+
+        .content {
+            display: flex;
+            margin-top: 10vh;
+            height: 50vh;
+            width: 50vh;
+            margin-left: auto;
+            margin-right: auto;
+            background-color: #ccc;
+            border-radius: 30px;
+        }
+
+        h2 {
+            margin-left: 20%;
+        }
+
+        form {
+            margin-top: 10%;
+            margin-left: auto;
+            margin-right: auto;
+
+        }
+
+        .champ {
+            padding-top: 5%;
+            border-radius: 10px;
+            height: 2vh;
+        }
+
+        .cellule {
+            padding-top: 2vh;
+        }
+
+        .btn {
+            width: 80%;
+            height: 4vh;
+            margin-left: 14%;
+            background-color: white;
+            color: green;
+            border-radius: 5px;
+            font-size: 1.5rem;
+        }
+
+        .btn:hover {
+            color: red;
+        }
+    </style>
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>saisi des donnees pour second degres</title>
-
 </head>
 
 <body>
-    <form action="index.php" method="POST">
-        <div>
-            <label for="a">a : </label>
-            <input id="a" name="a" type="number">
-        </div>
+    <div class="content">
+        <form action="index.php" method="POST">
+            <h2>ax²+bx+c=0</h2>
+            <div class="cellule">
+                <label for="a">a : </label>
+                <input class="champ" id="a" name="a" type="number" placeholder="           ax²+bx+c=0">
+            </div>
 
-        <div>
-            <label for="b">b : </label>
-            <input id="b" name="b" type="number">
-        </div>
+            <div class="cellule">
+                <label for="b">b : </label>
+                <input class="champ" id="b" name="b" type="number" placeholder="           ax²+bx+c=0">
+            </div>
 
-        <div>
-            <label for="c">c : </label>
-            <input id="c" name="c" type="number">
-        </div>
+            <div class="cellule">
+                <label for="c">c : </label>
+                <input class="champ" id="c" name="c" type="number" placeholder="           ax²+bx+c=0">
+            </div>
 
-        <div>
-            <input type="submit" value="calculer">
-        </div>
-    </form>
+            <div class="cellule ">
+                <input class="btn" type="submit" value="calculer">
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
--- exercice6/index.php
+++ exercice6/index.php
@@ -1,18 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>acceil second derges</title>
+    <style>
+        .content {
+            margin-top: 10vh;
+            height: 50vh;
+            background-color: red;
+        }
+    </style>
 </head>
+
 <body>
-<a href="form.html.php">formulaire de calcul</a>
-   <h2>a = <?php echo $_GET['a']; ?> </h2>
-   <h2>b = <?php echo $_GET['b']; ?> </h2>
-   <h2>c = <?php echo $_GET['c']; ?> </h2>
-<?php
-include 'fonctions.php';
-second_degre($_GET['a'],$_GET['b'],$_GET['c'])."\n";
-?>
-<a href="form.html.php">formulaire de calcul</a>
+    <div class="container">
+        <a href="form.html.php">formulaire de calcul</a>
+    </div>
+        <h2>a = <?php echo $_POST['a']; ?> </h2>
+        <h2>b = <?php echo $_POST['b']; ?> </h2>
+        <h2>c = <?php echo $_POST['c']; ?> </h2>
+        <?php
+        include 'fonctions.php';
+        if (!empty($_POST['a']) && !empty($_POST['b']) && !empty($_POST['c'])) {
+            second_degre($_POST['a'], $_POST['b'], $_POST['c']) . "\n";
+        } else {
+            echo "veilez remplir tous les champ !\n";
+        }
+        ?>
+</body>
+
+</html>
