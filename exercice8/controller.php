7f32f65b98b835cc668c3b623f0d824955b90aa3
khadim Niass <niassssn@gmail.com>
2022-02-08 12:36:27 +0000

    exercie 10 v1

--- exercice8/controller.php
+++ exercice8/controller.php
@@ -1,15 +1,15 @@
 <?php
-    include_once 'fonctions.php';
-    include_once '../ERROR_MANAGER.php';
-    if (isset($_POST['btn_display'])) {
-        if (is_numeric($_POST["N"]) && is_positif($_POST["N"])) {
-            ?><ul>
+include_once 'fonctions.php';
+include_once '../ERROR_MANAGER.php';
+if (isset($_POST['btn_display'])) {
+    if (is_numeric($_POST["N"]) && is_positif($_POST["N"])) {
+?><ul>
             <?php gen_number($_POST["N"]); ?>
-              </ul>
-        <?php
-        } else {
-            // echo "veillez entrer un nombre ! positif";
-            header('location:index.php');
-            die();
-        }
-    } 
+        </ul>
+<?php
+    } else {
+        // echo "veillez entrer un nombre ! positif";
+        header('location:index.php');
+        die();
+    }
+}
