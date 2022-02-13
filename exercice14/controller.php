74f2a71aabcebbe18baab824b5ef56547d911bf6
khadim Niass <niassssn@gmail.com>
2022-02-13 23:37:33 +0000

    exercice 14

--- exercice14/controller.php
+++ exercice14/controller.php
@@ -11,41 +11,43 @@ $org = 0;
 $free = 0;
 $exp = 0;
 $pro = 0;
-if (isset($_POST['btn-textarea']) && !empty($_POST['btn-textarea'])) {
-    $allNumb = array_map('trim', explode("\n", $numb));
-    for ($i = 0; $i < count($allNumb); $i++) {
-        if (is_numeric($allNumb[$i]) && strlen($allNumb[$i]) == 9 && (substr($allNumb[$i], 0, 2) == "77" || substr($allNumb[$i], 0, 2) == "78" || substr($allNumb[$i], 0, 2) == "76" || substr($allNumb[$i], 0, 2) == "70" || substr($allNumb[$i], 0, 2) == "75")) {
-            $numb_correct[$ic] = $allNumb[$i];
-            $ic++;
-        } else {
-            $numb_incorrect[$iinc] = $allNumb[$i];
-            $iinc++;
+if (isset($_POST['btn-textarea'])) {
+    if (isset($numb) && !empty($numb)) {
+        $allNumb = array_map('trim', explode("\n", $numb));
+        for ($i = 0; $i < count($allNumb); $i++) {
+            if (is_numeric($allNumb[$i]) && strlen($allNumb[$i]) == 9 && (substr($allNumb[$i], 0, 2) == "77" || substr($allNumb[$i], 0, 2) == "78" || substr($allNumb[$i], 0, 2) == "76" || substr($allNumb[$i], 0, 2) == "70" || substr($allNumb[$i], 0, 2) == "75")) {
+                $numb_correct[$ic] = $allNumb[$i];
+                $ic++;
+            } else {
+                $numb_incorrect[$iinc] = $allNumb[$i];
+                $iinc++;
+            }
         }
-    }
-    for ($i = 0; $i < count($numb_correct); $i++) {
-        if ((substr($numb_correct[$i], 0, 2) == "77") || substr($numb_correct[$i], 0, 2) == "78") {
-            $org++;
-        } elseif (substr($numb_correct[$i], 0, 2) == "76") {
-            $free++;
-        } elseif (substr($numb_correct[$i], 0, 2) == "70") {
-            $exp++;
-        } else {
-            $pro++;
+        for ($i = 0; $i < count($numb_correct); $i++) {
+            if ((substr($numb_correct[$i], 0, 2) == "77") || substr($numb_correct[$i], 0, 2) == "78") {
+                $org++;
+            } elseif (substr($numb_correct[$i], 0, 2) == "76") {
+                $free++;
+            } elseif (substr($numb_correct[$i], 0, 2) == "70") {
+                $exp++;
+            } else {
+                $pro++;
+            }
         }
+        $operateur = [
+            'orange' => $org,
+            'free' => $free,
+            'expresso' => $exp,
+            'pro mobile' => $pro
+        ];
+        $_SESSION['operateur'] = $operateur;
+        $_SESSION['numb_correct'] = $numb_correct;
+        $_SESSION['numb_incorrect'] = $numb_incorrect;
+        header('location:index.php');
+        die();
+    } else {
+        $_SESSION['message_error'] = "veillez replir le champ.";
+        header('location:index.php');
+        die();
     }
-    $operateur=[
-        'orange'=>$org,
-        'free'=>$free,
-        'expresso'=>$exp,
-        'pro mobile'=>$pro
-    ];
-    $_SESSION['operateur']=$operateur;
-    $_SESSION['numb_correct'] = $numb_correct;
-    $_SESSION['numb_incorrect'] = $numb_incorrect;
-    header('location:index.php');
-    die();
-} else {
-    $_SESSION['message_error'] = "veillez replir le champ.";
-    header('location:index.php');
-    die();
 }
--- exercice14/index.php
+++ exercice14/index.php
@@ -15,43 +15,52 @@ include_once '../ERROR_MANAGER.php';
 
 <body>
     <form action="controller.php" method="post">
-        <label for="text">remplir les numeros</label> <br>
-        <textarea name="numero" id="text" cols="30" rows="10" placeholder="list de numero"></textarea><br>
-        <input type="submit" name="btn-textarea" value="ENVOYER">
+        <div class="numero">
+            <label for="text" style="margin: 50px;">remplir les numeros</label> <br>
+            <textarea name="numero" id="text" cols="30" rows="10" placeholder="list de numero"></textarea><br>
+            <input class="btn" type="submit" name="btn-textarea" value="ENVOYER">
+        </div>
     </form>
-<?php
+    <?php
     if (isset($_SESSION['message_error'])) {
-    echo $_SESSION['message_error'];
-}?>
+        echo "<span class='error'>".$_SESSION['message_error']."</span>";
+    } ?>
 
 
-    <div>
+    <div class="output">
         <?php
+        echo "<div class='correct'>";
         if (isset($_SESSION['numb_correct'])) {
             echo "numéro correct <br>";
-            echo "<textarea style='color:green;' name='' id='' cols='' rows='10' readonly>";
+            echo "<textarea style='color:green;' name='' id='' cols='35' rows='10' readonly>";
             foreach ($_SESSION['numb_correct'] as $key => $value) {
-                echo $value." ";
+                echo $value . " ";
             }
             echo "</textarea> <br>";
         }
+        echo "</div>";
 
+        echo "<div class='incorrect'>";
         if (isset($_SESSION['numb_incorrect'])) {
             echo "numéro incorrect <br>";
-            echo "<textarea style='color:red;' name='' id='' cols='' rows='10' readonly>";
+            echo "<textarea style='color:red;' name='' id='' cols='35' rows='10' readonly>";
             foreach ($_SESSION['numb_incorrect'] as $key => $value) {
-                echo $value ." ";
+                echo $value . " ";
             }
             echo "</textarea> <br>";
         }
+        echo "</div>";
+
+        echo "<div class='operateur'>";
         if (isset($_SESSION['operateur'])) {
             echo "détail opérateur <br>";
-            echo "<textarea readonly> ";
+            echo "<textarea cols='40' rows='10' readonly> ";
             foreach ($_SESSION['operateur'] as $key => $value) {
-                echo $key." a ".$value." numéros  ;";
+                echo $key . " a " . $value . " numéros  ;";
             }
             echo "</textarea>";
         }
+        echo "</div>";
         if (isset($_SESSION['operateur'])) {
             unset($_SESSION['operateur']);
         }
@@ -62,7 +71,7 @@ include_once '../ERROR_MANAGER.php';
         if (isset($_SESSION['numb_incorrect'])) {
             unset($_SESSION['numb_incorrect']);
         }
- 
+
         if (isset($_SESSION['message_error'])) {
             unset($_SESSION['message_error']);
         }
